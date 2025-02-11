d503ba6bc6b087b2efb774dcc387814fd417d680
heeyeon9578 <heeyeon9578@naver.com>
2025-02-11 23:25:57 +0900

    bug: home.tsx 충돌병합해결

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,14 @@
 // Home.tsx 테스트
 import React,{useState} from 'react';
-import { View, Text } from 'react-native';
-import BasicButton from '../components/BasicButton';
-import BasicInput from '../components/BasicInput';
-import BasicText from '../components/BasicText';
-import SVGButton from '../components/SVGButton';
-import SVGTextButton from '../components/SVGTextButton';
+import { View } from 'react-native';
+
 import styles from '../styles/Home.style';
+import BasicButton from '../components/BasicButton';
+import BasicText from '../components/BasicText';
 
 
 const Home: React.FC = () => {
-
-  const [loading, setLoading] = useState(false);
-  const [name, setName] = useState('');
-
+  const [loading, setLoading] = useState<boolean>(false);
   const handlePress = () => {
     setLoading(true);
     // 버튼 클릭 시 원하는 로직을 수행하고, 완료 후 로딩 상태를 false로 전환합니다.
@@ -24,76 +19,15 @@ const Home: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>홈 화면</Text>
-      <Text style={styles.subtitle}>환영합니다! 여기는 메인 스크린입니다.</Text>
       <BasicButton
-        text="버튼 텍스트"
+        text="어플 공지사항/안내"
         onPress={handlePress}
         loading={loading}
-        // disabled={true}
-        // 활성화 상태 색상 객체 (배경, 텍스트, 테두리)
-        // enabledColors={{
-        //   backgroundColor: '#FFFFFF',
-        //   textColor: '#000000',
-        //   borderColor: '#000000',
-        // }}
-        // 비활성화 상태 색상 객체
-        // disabledColors={{
-        //   backgroundColor: '#D3D3D3',
-        //   textColor: '#000000',
-        //   borderColor: '#D3D3D3',
-        // }}
-        // 추가 스타일 (필요에 따라 조정)
-        buttonStyle={styles.button}
-        // textStyle={styles.text}
-        // accessibilityLabel="Example Button"
+        buttonStyle={styles.notiButton}
+        textStyle={styles.notiText}
       />
-
-      <BasicInput
-        text="이름을 입력하세요"  // placeholder로 사용됨
-        value={name}
-        onChangeText={setName}
-      />
-
-    <BasicText text="text prop을 사용한 예시입니다.22" />
-
-    <SVGButton
-        iconName="Notification" // ../assets/svg/index.ts 에서 export한 아이콘 이름
-        onPress={handlePress}
-        loading={loading}
-        // enabledColors={{
-        //   backgroundColor: '#FFFFFF',
-        //   textColor: '#000000',
-        //   borderColor: '#000000',
-        // }}
-        // disabledColors={{
-        //   backgroundColor: '#D3D3D3',
-        //   textColor: '#000000',
-        //   borderColor: '#D3D3D3',
-        // }}
-        //buttonStyle={styles.button}
-        accessibilityLabel="알림 버튼"
-      />
-
-      <SVGTextButton
-        iconName="Notification" // 'BellIcon'은 '../assets/svg/index.ts'에서 export한 아이콘 키
-        text="알림"
-        onPress={handlePress}
-        loading={loading}
-        enabledColors={{
-          backgroundColor: '#FFFFFF',
-          textColor: '#000000',
-          borderColor: '#000000',
-        }}
-        disabledColors={{
-          backgroundColor: '#D3D3D3',
-          textColor: '#000000',
-          borderColor: '#D3D3D3',
-        }}
-        //buttonStyle={styles.button}
-        //textStyle={styles.text}
-        accessibilityLabel="알림 버튼"
-      />
+      <BasicText text="갈대 시작하기" style={styles.startGaldae}/>
+      <BasicText text="목적지 설정 후 동승자를 구하세요!" style={styles.startGaldaeEx}/>
     </View>
   );
 };
